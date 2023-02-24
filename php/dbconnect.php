6b62b9f7fec2257a395cacdc2668abc01f0f9d2d
Izzah Rodzi <izzahrodzi12@gmail.com>
2023-02-24 14:41:30 +0800

    Add files via upload

---
+++ php/dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(0);
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "homestay_raya";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
